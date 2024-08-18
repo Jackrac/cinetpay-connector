1e960249d5e993e57f0895dba58ad90fb812db14
Ikki46 <biscreativa@gmail.com>
2024-08-18 22:06:24 +0000

    Update pay.php

--- pay.php
+++ pay.php
@@ -1,4 +1,5 @@
 <?php
+
     require 'utils.php';
 ?>
 
@@ -22,10 +23,10 @@
             "transaction_id" => strval(floor(((float)rand()/(float)getrandmax()) * 100000000)),
             "amount" => $amount,
             "currency" => "XOF",
-            "description" =>  " TEST INTEGRATION ", // PRODUCTION ( Apres avant la mise en PROD)
+            "description" =>  " EAA PAIEMENT ", // PRODUCTION ( Apres avant la mise en PROD)
             "customer_id" =>  $entryid,
             "notify_url" =>  "https://eaa.ci/paiement/notify.php",
-            "return_url" =>  "https://eaa.ci/paiement/feedback.php?entryid=" . $entryid,
+            "return_url" =>  "https://eaa.ci/paiement-en-ligne-ext",
             "channels" =>  "ALL",
             "lang" =>  "FR",
             "metadata" => $entryid
@@ -38,10 +39,7 @@
 ?>
 
 <?php if($error === ''): ?>
-    <iframe src="<?= $payment_url ?>" title="CinetPay Page"></iframe>
+    <iframe src="<?= $payment_url ?>" title="CinetPay Page" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>
 <?php else: ?>
     <span><?= $error ?></span>
 <?php endif; ?>
-
-
-
